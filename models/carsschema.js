4fae365feb7ecff5d84e35a991218ec1ac107f39
Aluka ~3 <rawaa2302751@miuegypt.edu.eg>
2025-06-04 12:46:25 +0300

    test1schemacar

--- models/carsschema.js
+++ models/carsschema.js
@@ -1,4 +1,4 @@
-const mongoose=require('mongoose')
+/*const mongoose=require('mongoose')
 const carSchema = new mongoose.Schema({
   brand: String,
   model: String,
@@ -6,4 +6,42 @@ const carSchema = new mongoose.Schema({
   color: String,
   price: Number,
   availability: { type: Boolean, default: true }
+});*/
+
+const mongoose = require('mongoose');
+
+const carsschema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    model: {
+        type: String,
+        required: true,
+        enum: ['Electric', 'Gas', 'Hybrid']
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    engine: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    availability: {
+        type: Boolean,
+        default: true
+    }
+}, {
+    //timestamps: true
 });
+
+module.exports = mongoose.model('Car', carsschema);
