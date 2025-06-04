3f7e234ff55700d1bd2f5267c1113bd2b52f333c
MalakElghamrawy <malak2301546@miuegypt.edu.eg>
2025-06-04 13:01:49 +0300

    homepagejs

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,3 +1,37 @@
+/*const video = document.getElementById("hero-video");
+const videos = [
+    "assests/videos/videoonespeed.mp4",
+    "assests/videos/lambodriving.mp4"
+];
+let currentVideoIndex = 0;
+
+video.addEventListener("ended", () => {
+    currentVideoIndex = (currentVideoIndex + 1) % videos.length;
+    video.src = videos[currentVideoIndex];
+video.play(); 
+});
+
+function playSound(soundFile) {
+const sound = new Audio(`assests/sounds/${soundFile}.mp3`); 
+    sound.play();
+}
+
+const bestSellerImages = [
+    "car2.png",  
+    "car12.png",
+    "car9.png" 
+];
+
+let currentBestSellerIndex = 0; 
+
+function changeBestSellerImage() {
+    const bestSellerImg = document.getElementById("best-seller-image");
+    
+    currentBestSellerIndex = (currentBestSellerIndex + 1) % bestSellerImages.length;
+
+    bestSellerImg.src = bestSellerImages[currentBestSellerIndex];
+}*/
+
 const video = document.getElementById("hero-video");
 const videos = [
     "assests/videos/videoonespeed.mp4",
@@ -31,4 +65,3 @@ function changeBestSellerImage() {
 
     bestSellerImg.src = bestSellerImages[currentBestSellerIndex];
 }
-
