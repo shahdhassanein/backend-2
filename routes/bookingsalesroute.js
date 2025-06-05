e0dacbcff726bf1b618fdcdec8650be5c87474f9
MalakElghamrawy <malak2301546@miuegypt.edu.eg>
2025-06-05 13:49:34 +0300

    Update bookingsalesroute.js

--- routes/bookingsalesroute.js
+++ routes/bookingsalesroute.js
@@ -2,3 +2,9 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/bookingsalescontroller');
 const auth = require('../middleware/auth');
+router.post('/purchase', auth, controller.purchaseCar);
+router.get('/my-bookings', auth, controller.getMyBookings);
+router.get('/my-purchases', auth, controller.getMyPurchases);
+router.put('/booking/:id/status', auth, controller.updateBookingStatus);
+
+module.exports = router;
