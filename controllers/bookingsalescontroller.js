f0a3ea461b2f736254dc0394efe7a3ce096734bc
MalakElghamrawy <malak2301546@miuegypt.edu.eg>
2025-06-05 13:46:12 +0300

    tryandcatchbookingsch

--- controllers/bookingsalescontroller.js
+++ controllers/bookingsalescontroller.js
@@ -1,13 +1,13 @@
 const Booking = require('../models/bookingschema'); 
 const Purchase = require('../models/purchaseschema');
-exports.purchaseCar = async (req, res) => {
-
-
-
-
-
-
-
-
-    
-}
+exports.purchaseCar = async (req, res) =>
+  {
+  try {
+    const { userId, carId, paymentInfo } = req.body;
+    const newPurchase = new Purchase({ userId, carId, paymentInfo });
+    await newPurchase.save();
+    res.status(201).json({ message: 'Car reserved successfully' });
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to reserve car' });
+  }
+};
