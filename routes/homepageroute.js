2c1121cfaeabf0fff03c526b823faa26ec596532
MalakElghamrawy <malak2301546@miuegypt.edu.eg>
2025-06-05 14:06:52 +0300

    Update homepageroute.js

---
+++ routes/homepageroute.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    res.status(200).json({
+      success: true,
+      message: 'Welcome to the API Home Page'
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: 'Server Error'
+    });
+  }
+});
+
+module.exports = router;
