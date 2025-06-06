efae2fe428b7a7769183ccdcca3a02ea69c06046
Aluka ~3 <rawaa2302751@miuegypt.edu.eg>
2025-06-06 12:40:51 +0300

    update

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -4,3 +4,23 @@ const generateToken = require('../utils/generateToken');
 // @desc    Auth user & get token
 // @route   POST /api/users/login
 
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Please provide email and password' });
+        }
+
+        const user = await User.findOne({ email }).select('+password');
+        
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        const isMatch = await user.matchPassword(password);
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        const token = generateToken(user._id);
