e385b3521432bfaafa2eb0643e93c08af594193b
Aluka ~3 <rawaa2302751@miuegypt.edu.eg>
2025-06-06 14:13:07 +0300

    Update carRoutes.js

--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Car = require('../models/Car');
+const Car = require('../models/carsschema');
 const { protect, authorize } = require('../middleware/auth');
 
 
