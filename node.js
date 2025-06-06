79749a9955c39b5cb540ff593b4709c4ad987b07
Aluka ~3 <rawaa2302751@miuegypt.edu.eg>
2025-06-06 14:10:26 +0300

    Update node.js

--- node.js
+++ node.js
@@ -1,13 +1,27 @@
 require('dotenv').config();
 
 const express=require('express');
+const connectDB = require('./config/db');
 const mongoose = require('mongoose');
+
 //tells the node.js to go to the routes files and bring whatever exported from it 
 const app = express();
 
 const carRoutes= require('./routes/carRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+connectDB();
+
+
+app.use(express.json());
+
+
+// hena al routes ya shabab add it hena 
 app.use('/',carRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(express.static('./public'));
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});//i added it 3ashan bas a3raf law server crashed aw l2 okay.
