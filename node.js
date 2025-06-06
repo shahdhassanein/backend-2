26dfc2754ee0672b570a203075a39b3463191b7b
Aluka ~3 <rawaa2302751@miuegypt.edu.eg>
2025-06-06 11:53:19 +0300

    correcting mistakes in node.js

--- node.js
+++ node.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 
 const express=require('express');
-const mongoose=require('monoose');
+const mongoose = require('mongoose');
 //tells the node.js to go to the routes files and bring whatever exported from it 
+const app = express();
+
 const carRoutes= require('./routes/carRoutes');
 app.use('/',carRoutes);
-const app=express();
+
 app.use(express.static('./public'));
 
 app.listen(3000);
