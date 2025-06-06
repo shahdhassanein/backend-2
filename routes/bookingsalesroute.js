0e95f9e5254775bfb873d48519b7f3f48b992d27
MalakElghamrawy <malak2301546@miuegypt.edu.eg>
2025-06-07 00:41:25 +0300

    Update bookingsalesroute.js

--- routes/bookingsalesroute.js
+++ routes/bookingsalesroute.js
@@ -6,5 +6,13 @@ router.post('/purchase', auth, controller.purchaseCar);
 router.get('/my-bookings', auth, controller.getMyBookings);
 router.get('/my-purchases', auth, controller.getMyPurchases);
 router.put('/booking/:id/status', auth, controller.updateBookingStatus);
+router.get('/view-my-bookings', auth, async (req, res) => {
+  try {
+    const bookings = await controller.getMyBookingsData(req.user.id);
+    res.render('mybookings', { bookings });  // ashan a pass el bookings to EJS
+  } catch (error) {
+    res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;
