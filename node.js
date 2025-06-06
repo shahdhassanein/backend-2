17d6d06a40188d89df82ae160c8e0552d60eb77f
Aluka ~3 <rawaa2302751@miuegypt.edu.eg>
2025-06-06 11:31:32 +0300

    dotenv

--- node.js
+++ node.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express=require('express');
 const mongoose=require('monoose');
 //tells the node.js to go to the routes files and bring whatever exported from it 
