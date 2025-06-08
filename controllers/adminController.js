0554f7d7b74ddf15b07856ba2a54aefc89cf9c75
kinaz-kk <kinaz2301458@miuegypt.edu.eg>
2025-06-08 20:39:38 +0300

    added to adminController
    get report + get all cars

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -14,11 +14,41 @@ const car = require ('../models/carsschema');
 
     //get all bookings
      exports.getAllBookings = async (req,res)=>{ 
-        try { const Bookings= await Bookings.find().populate('user car');
+        try { const Bookings= await Bookings.find().populate('userId').populate ('carId');
             res.json(Bookings);
         } catch(error) {
             res.status (500).json ({message: 'failed to fetch bookings'});
         }
      }; 
 
-     
+       //get all cars
+       exports.getAllCars = async (req,res) =>{
+        try{
+            const cars = await CaretPosition.find();
+            res.json(cars); 
+        } catch(error) {
+            res.status(500).json({message:'failed to fetch cars'});
+        }
+       };
+
+
+       //get dashboard report
+       exports.getReport = async (req,res) => {
+        try {
+            const totalUsers = await User.countDocuments();
+            const totalCars = await Booking.countDocuments();
+            const recentBookings = await Booking.find().sort ({ createdAt: -1}). limit (100).populate('userId').populate('carId');
+
+            res.json ({ stats:{ 
+                totalUsers,
+                totalCars,
+                totalBookings
+            },
+             recentBookings,
+        });
+         } catch(error) {
+            res.status(500).json({message:'failed to generate report'});
+         }
+        };
+
+       
