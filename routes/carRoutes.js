feca39888915276276015bd1beb0377b422570f4
MalakElghamrawy <malak2301546@miuegypt.edu.eg>
2025-06-09 01:11:53 +0300

    Update carRoutes.js

--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -8,4 +8,9 @@ router.get('/cars/:id', carController.getCarById);
 // router.post('/cars', upload.array('images'), carController.createCar);
 router.put('/cars/:id', carController.updateCar);
 router.delete('/cars/:id', carController.deleteCar);
+<<<<<<< Updated upstream
 module.exports = router;  // <<-- important export
+=======
+
+module.exports = router;  
+>>>>>>> Stashed changes
