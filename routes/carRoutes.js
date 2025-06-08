b978483407771bf809e2af9ee627dc76012de503
MalakElghamrawy <malak2301546@miuegypt.edu.eg>
2025-06-08 13:27:36 +0300

    Update carRoutes.js

--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const Car = require('../models/carsschema');
 const { protect, authorize } = require('../middleware/auth');
-const carController=require('../controllers/carController');
-router.get('/cars',carController.getAllCars);
-router.get('/cars/:id',carController.getCarById);
-//router.post('/cars',upload.array('images'),carController.createCar);
-router.put('cars/:id',carController.updateCar);
-router.delete('/cars/:id',carController.deleteCar);
+const carController = require('../controllers/carController');
+
+router.get('/cars', carController.getAllCars);
+router.get('/cars/:id', carController.getCarById);
+// router.post('/cars', upload.array('images'), carController.createCar);
+router.put('/cars/:id', carController.updateCar);
+router.delete('/cars/:id', carController.deleteCar);
+
+module.exports = router;  // <<-- important export
