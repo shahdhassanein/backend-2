f101123088b380b83ddaa9e669b186298c637d93
MalakElghamrawy <malak2301546@miuegypt.edu.eg>
2025-06-10 22:55:17 +0300

    Update carRoutes.js

--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const carController = require('../controllers/carController');
-
+const { protect, admin } = require('../middleware/auth');
 // Handle POST request to add a car
 router.post('/addcar', carController.addCar);
 module.exports = router;
