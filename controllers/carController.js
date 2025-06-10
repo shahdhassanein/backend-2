8ea95991f11163ef3134f7524446fb82b7631e52
shahdhassanein <shahd2302021@miuegypt.edu.eg>
2025-06-10 23:18:07 +0300

    .

--- controllers/carController.js
+++ controllers/carController.js
@@ -1,14 +1,26 @@
 const Car = require('../models/carsschema');
 
+const getAllCars = async (req, res) => {
+  try {
+    const cars = await Car.find();
+    res.render('carllisting', { title: 'Car Listing', cars });
+  } catch (err) {
+    res.status(500).send('Error getting cars: ' + err.message);
+  }
+};
+
 const addCar = async (req, res) => {
   try {
     const { name, model, price, engine, color, image } = req.body;
     const newCar = new Car({ name, model, price, engine, color, image });
     await newCar.save();
-    res.redirect('/add-car'); // or use '/carlisting'
+    res.redirect('/carllisting');
   } catch (err) {
     res.status(500).send('Error saving car: ' + err.message);
   }
 };
 
-module.exports = { addCar };
+module.exports = {
+  getAllCars,
+  addCar
+};
--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -1,13 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { addCar } = require('../controllers/carController');
+const { addCar } = require('../controllers/carController'); // Import the addCar controller
 router.post('/addcar', addCar);
-
-// Handle POST request to add a car
-router.post('/addcar', carController.addCar);
-router.get('/', carController.getAllCars);
-router.get('/:id', carController.getCarById);
-router.post('/add', protect, admin, carController.createCar);
-router.put('/:id', protect, admin, carController.updateCar); 
-router.delete('/:id', protect, admin, carController.deleteCar);
 module.exports = router;
