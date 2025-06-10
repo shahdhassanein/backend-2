01d0cfc8453d2ea0ec82501485562c808eec03c0
shahdhassanein <shahd2302021@miuegypt.edu.eg>
2025-06-10 22:54:21 +0300

    .

--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -5,4 +5,9 @@ router.post('/addcar', addCar);
 
 // Handle POST request to add a car
 router.post('/addcar', carController.addCar);
+router.get('/', carController.getAllCars);
+router.get('/:id', carController.getCarById);
+router.post('/add', protect, admin, carController.createCar);
+router.put('/:id', protect, admin, carController.updateCar); 
+router.delete('/:id', protect, admin, carController.deleteCar);
 module.exports = router;
