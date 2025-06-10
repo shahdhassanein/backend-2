7614655a35ff6bb82d67ee497830d38b30f23ffd
shahdhassanein <shahd2302021@miuegypt.edu.eg>
2025-06-10 14:56:10 +0300

    carlisting routes

--- app.js
+++ app.js
@@ -21,31 +21,32 @@ console.log('bookingsalesroute:', bookingsalesroute);*/ //hsybo dlw ashan nhdd f
 app.use(express.json());
 app.use(express.static('./public'));
 // hena al routes ya shabab add it hena 
-app.use('/admin', adminRoutes);
-app.use('/api/cars',carRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/bookingsales', bookingsalesroute);//purchaseroute
+//app.use('/admin', adminRoutes);
+//app.use('/api/cars',carRoutes);
+//app.use('/api/users', userRoutes);
+//app.use('/api/bookingsales', bookingsalesroute);//purchaseroute
 app.use(express.static('./public'));
 app.set('view engine', 'ejs'); 
 app.set('views', path.join(__dirname,'views'));
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get ('/',(req,res)=>{res.render('homepage',{title: 'Home Page'})});
+app.get('/usersmangment',(req,res)=>{res.render ('usersmangment',{title:'usersmangment'})});
 app.get ('/admin',(req,res)=>{res.render('admin',{title: 'admin page'})});
+
 app.get ('/mypurchases', (req,res)=>{res.render ('purchases',{title:'Purchases'})});
 app.get ('/Dashboard',(req,res)=>{res.render ('Dashboard',{title:'Dashboard'})});
 app.get ('/Contact', (req,res)=> {res.render('Contact', {title:'Contact'})});
 app.get ('/checkout', (req,res)=> {res.render ('checkout',{title:'Checkout'})});
 app.get ('/cart', (req,res)=> {res.render ('cart', {title:'Cart'})});
 
+app.get ('/carllisting.ejs', (req, res)=> {res.render ('carllisting', {title:'Car Listing'})});
 app.get ('/carllisting', (req, res)=> {res.render ('carllisting', {title:'Car Listing'})});
 app.get ('/admin-orders', (req,res)=>{res.render ('admin-orders', {title:'Admin Orders'})});
 app.get ('/inventory',(req,res)=>{res.render('inventory', {title: 'Inventory'})});
 app.get ('/login', (req,res)=>{ res.render ('login', {title:'Login'})});
 app.get ('/Privacy', (req,res)=> {res.render ('Privacy', {title:'Privacy'})});
 app.get ('/Term', (req,res)=> {res.render ('Term', {titl:'Term'})});
-
-
 app.post('/api/cars', (req, res) => {
     const newCar = req.body;
     
