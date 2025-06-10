54eb28cb4362100a9614594248c41be5fe4748bb
MalakElghamrawy <malak2301546@miuegypt.edu.eg>
2025-06-10 22:49:36 +0300

    Update carRoutes.js

--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -4,4 +4,9 @@ const carController = require('../controllers/carController');
 
 // Handle POST request to add a car
 router.post('/addcar', carController.addCar);
+router.get('/', carController.getAllCars);
+router.get('/:id', carController.getCarById);
+router.post('/add', protect, admin, carController.createCar);
+router.put('/:id', protect, admin, carController.updateCar); 
+router.delete('/:id', protect, admin, carController.deleteCar);
 module.exports = router;
